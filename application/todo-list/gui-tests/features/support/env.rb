49350fea3bd24087d5c56bf92543f161567eb32c
Tomasz Wierzchowski <twierzchowski@future-processing.com>
2015-05-01 16:33:33 +0200

    update to selenium 2.45

--- application/todo-list/gui-tests/features/support/env.rb
+++ application/todo-list/gui-tests/features/support/env.rb
@@ -4,3 +4,7 @@ Capybara.run_server = false
 Capybara.default_driver = :selenium
 Capybara.default_selector = :css
 Capybara.default_wait_time = 5
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+end
